5a7883bbb11f513fb5ca10e2e03ad8430737624e
Emiton Alves <emiton_alves@my.uri.edu>
2019-12-02 22:21:27 -0500

    Added basic seaborn visualizations

--- spotipy_api.py
+++ spotipy_api.py
@@ -1,5 +1,8 @@
 import json # TODO: Remove
+import matplotlib.pyplot as plt
 import os
+import pandas as pd
+import seaborn as sns
 import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials # For Spotify API use
 
@@ -25,7 +28,8 @@ def main():
     for playlist in playlists_to_analyze:
         get_playlist_audio_features(playlist)
 
-    # playlist_info = get_playlist_audio_features('spotify:playlist:37i9dQZF1DX0XUsuxWHRQd')
+    visualize_playlist_data(all_playlists['RapCaviar'])
+
 
 
 def get_playlist_audio_features(uri):
@@ -37,8 +41,8 @@ def get_playlist_audio_features(uri):
     playlist_name = results['name']
 
     all_playlists[playlist_name] = {}
-    all_playlists[playlist_name]['name'] = []
-    all_playlists[playlist_name]['track uri'] = []
+    # all_playlists[playlist_name]['name'] = []
+    # all_playlists[playlist_name]['track uri'] = []
     all_playlists[playlist_name]['acousticness'] = []
     all_playlists[playlist_name]['danceability'] = []
     all_playlists[playlist_name]['energy'] = []
@@ -46,9 +50,10 @@ def get_playlist_audio_features(uri):
     all_playlists[playlist_name]['liveness'] = []
     all_playlists[playlist_name]['loudness'] = []
     all_playlists[playlist_name]['speechiness'] = []
-    all_playlists[playlist_name]['tempo'] = []
+    # all_playlists[playlist_name]['tempo'] = []
     all_playlists[playlist_name]['valence'] = []
-    all_playlists[playlist_name]['popularity'] = []
+    # TODO: Add popularity later
+    # all_playlists[playlist_name]['popularity'] = []
 
     for track_metadata in results['tracks']['items']:
         # DEBUG STATEMENT
@@ -59,8 +64,8 @@ def get_playlist_audio_features(uri):
             name = track_metadata['track']['name']
             print(name)
             track_uri = track_metadata['track']['uri']
-            all_playlists[playlist_name]['name'].append(name)
-            all_playlists[playlist_name]['track uri'].append(track_uri)
+            # all_playlists[playlist_name]['name'].append(name)
+            # all_playlists[playlist_name]['track uri'].append(track_uri)
 
             # extract features
             features = sp.audio_features(track_uri)
@@ -71,10 +76,32 @@ def get_playlist_audio_features(uri):
             all_playlists[playlist_name]['liveness'].append(features[0]['liveness'])
             all_playlists[playlist_name]['loudness'].append(features[0]['loudness'])
             all_playlists[playlist_name]['speechiness'].append(features[0]['speechiness'])
-            all_playlists[playlist_name]['tempo'].append(features[0]['tempo'])
+            # all_playlists[playlist_name]['tempo'].append(features[0]['tempo'])
             all_playlists[playlist_name]['valence'].append(features[0]['valence'])
 
     return results
+
+
+def visualize_playlist_data(playlist):
+    # create dataframe
+    df = pd.DataFrame.from_dict(playlist)
+    fig = plt.figure()
+
+    # sns.distplot(df['acousticness'], hist='true', kde='false', bins=25)
+    # sns.distplot(df['danceability'], hist='true', kde='false', bins=25)
+    # sns.distplot(df['energy'], hist='true', kde='false', bins=25)
+    # sns.distplot(df['instrumentalness'], hist='true', kde='false', bins=25)
+    # sns.distplot(df['liveness'], hist='true', kde='false', bins=25)
+    # sns.distplot(df['loudness'], hist='true', kde='false', bins=25)
+    # sns.distplot(df['speechiness'], hist='true', kde='false', bins=25)
+    # sns.distplot(df['tempo'], hist='true', kde='false', bins=25)
+    # sns.distplot(df['valence'], hist='true', kde='false', bins=25)
+
+    df.boxplot()
+
+    plt.show()
+    return ""
+
 
 if __name__ == '__main__':
         main()
